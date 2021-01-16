c31b5c3cec4d14b6a57477870624f2213a8b7f3b
tangyun <tangyun1992@qq.com>
2021-01-16 21:40:57 +0800

    使用koa-json-error中间件完成自动错误处理，并配置生产环境不显示stack

--- app/index.js
+++ app/index.js
@@ -1,18 +1,16 @@
 const Koa = require('koa')
 const app = new Koa()
 const bodyparser = require('koa-bodyparser')
+const error = require('koa-json-error')
 const routing = require('./routes')
 
-app.use(async (ctx, next) => {
-  try {
-    await next()
-  } catch (err) {
-    ctx.status = err.status || err.statusCode || 500
-    ctx.body = {
-      message: err.message
-    }
-  }
-})
+app.use(
+  error({
+    postFormat: (e, { stack, ...rest }) =>
+      process.env.NODE_ENV === 'production' ? rest : { stack, ...rest },
+  })
+)
+
 app.use(bodyparser())
 routing(app)
 
